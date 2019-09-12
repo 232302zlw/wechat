8cb460f8773cf281e592a783821bfc3da1dbf01e
232302zlw <1160687529@qq.com>
2019-09-12 15:32:24 +0800

    first

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -9,6 +9,17 @@ class EventController extends Controller
 {
     public function event()
     {
-        echo $_GET['echostr'];
+//        dd($_POST);
+        $xml_string = file_get_contents('php://input'); // 获取
+        $wechat_log_path = storage_path('logs/wecchat/'.date("Y-m-d").'.log');
+        file_put_contents($wechat_log_path,"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n",FILE_APPEND);
+        file_put_contents($wechat_log_path,$xml_string,FILE_APPEND);
+        file_put_contents($wechat_log_path,"\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n",FILE_APPEND);
+
+//        dd($xml_string);
+        $xml_obj = simplexml_load_string($xml_string,'SimpleXMLElement',LIBXML_NOCDATA);
+        $xml_arr = (array)$xml_obj;
+        \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
+//        echo $_GET['echostr'];
     }
 }
