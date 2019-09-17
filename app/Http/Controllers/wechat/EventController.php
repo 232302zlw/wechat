249cd3fb5e08aadff88b9aa0a650a3592140f180
232302zlw <1160687529@qq.com>
2019-09-17 11:14:40 +0800

    first

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -22,5 +22,24 @@ class EventController extends Controller
         $xml_arr = (array)$xml_obj;
         \Log::Info(json_encode($xml_arr,JSON_UNESCAPED_UNICODE));
 //        echo $_GET['echostr'];
+        // 业务逻辑（防止刷业务）
+        if ($xml_arr['MsgType'] == 'event') {
+            if ($xml_arr['Event'] == 'subscribe') {
+                $share_code = explode('_',$xml_arr['EventKey'])[1];
+                $user_openid = $xml_arr['FromUserName']; // 粉丝的openid
+                // 判断是否已经在日志里
+                $wechat_openid = DB::table('wechat_openid')->where(['openid',$user_openid])->first();
+                if (empty($wechat_openid)) {
+                    Users::where('id',$share_code)->increment('share_num',1);
+                    DB::table('wechat_openid')->insert([
+                        'openid' => $user_openid,
+                        'add_time' => time()
+                    ]);
+                }
+            }
+        }
+        $message = '欢迎关注！大爷常来玩啊！';
+        $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
+
     }
 }
