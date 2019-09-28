2c5f6d10c568b1a7d24c697fb2676d248bc3e3ac
232302zlw <1160687529@qq.com>
2019-09-28 11:32:46 +0800

    last

--- app/Http/Controllers/wechat/EventController.php
+++ app/Http/Controllers/wechat/EventController.php
@@ -31,7 +31,7 @@ class EventController extends Controller
 //            ]);
 //            dd($aa);
                 $user_info = DB::table('wechat_openid')->where(['openid'=>$xml_arr['FromUserName']])->first();
-                dd($user_info);
+//                dd($user_info);
                 $message = 'hello'.$user_info->nickname;
                 $xml_str = '<xml><ToUserName><![CDATA['.$xml_arr['FromUserName'].']]></ToUserName><FromUserName><![CDATA['.$xml_arr['ToUserName'].']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['.$message.']]></Content></xml>';
                 echo $xml_str;
